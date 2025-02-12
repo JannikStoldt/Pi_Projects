97e7854c6ceff802937c76c4844cd930830658ec
JannikStoldt <jannik.stoldt@gmx.de>
2025-02-12 20:55:20 +0100

    'updates

--- scripts/powershell_git.py
+++ scripts/powershell_git.py
@@ -9,10 +9,10 @@ commands = [
 
 # Befehle der Reihe nach ausführen
 for command in commands:
-    print(f"Führe aus: {command}")
+    #print(f"Führe aus: {command}")
     result = subprocess.run(command, shell=True, capture_output=True, text=True)
     
     # Ausgabe des Befehls anzeigen
-    print("Ergebnis:")
-    print(result.stdout)  # Ausgabe des Befehls
+    #print("Ergebnis:")
+    #print(result.stdout)  # Ausgabe des Befehls
     print(result.stderr)  # Fehlerausgabe, falls vorhanden
