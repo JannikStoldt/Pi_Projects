8215358d82cfa190672b83a9efd84139ef4adb47
JannikStoldt <jannik.stoldt@gmx.de>
2025-02-12 20:23:38 +0100

    test

--- test.py
+++ test.py
@@ -13,16 +13,12 @@ ret, frame = cap.read()
 
 # Überprüfe, ob das Bild erfolgreich gelesen wurde
 if ret:
-    # Zeige das Bild in einem Fenster an
-    cv2.imshow("Webcam Bild", frame)
 
     # Speichere das Bild auf der Festplatte
     cv2.imwrite('webcam_bild.jpg', frame)
     print("Bild wurde gespeichert.")
 
 # Warte, bis eine Taste gedrückt wird, und schließe dann das Fenster
-cv2.waitKey(0)
-cv2.destroyAllWindows()
 
 # Release die Webcam
 cap.release()
